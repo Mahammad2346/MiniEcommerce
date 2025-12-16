2c790771cccbbb52051c378705f2d4a872807123
mutsuhito250 <echoofmeaning2025@gmail.com>
2025-12-16 09:53:15 +0100

    Rename layers to classic DataAccess and BUsinessLogic naming

---
+++ MiniEcommerce.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniEcommerce.Domain.Entities;
+
+namespace MiniEcommerce.Infrastructure.Persistence
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}

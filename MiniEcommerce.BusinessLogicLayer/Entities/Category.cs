60b7e1dd00cb72adf46446a80d6bdfb133dacbf1
mutsuhito250 <echoofmeaning2025@gmail.com>
2025-12-17 12:23:30 +0100

    Add Product and Category entities

---
+++ MiniEcommerce.BusinessLogicLayer/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.BusinessLogicLayer.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        
+        public ICollection<Product> Products { get; set; } = new List<Product>();   
+    }
+}
---
+++ MiniEcommerce.BusinessLogicLayer/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.BusinessLogicLayer.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}

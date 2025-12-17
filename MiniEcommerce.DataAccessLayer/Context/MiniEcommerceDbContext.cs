704b83f985c9b383f117bd00a0864e6f38b2a4e4
mutsuhito250 <echoofmeaning2025@gmail.com>
2025-12-17 10:11:22 +0100

    Add base DbContext to DataAccessLayer

---
+++ MiniEcommerce.DataAccessLayer/Context/MiniEcommerceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.DataAccessLayer.Context
+{
+    public class MiniEcommerceDbContext : DbContext
+    {
+        public MiniEcommerceDbContext(DbContextOptions<MiniEcommerceDbContext>options) : base(options)
+        {
+                
+        }
+    }
+}

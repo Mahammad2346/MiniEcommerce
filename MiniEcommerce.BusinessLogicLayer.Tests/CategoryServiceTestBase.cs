3689378451e63118b805c5676e81d4d00d7fb827
Mahammad Ismayilov <mahammad.ismayilov.2025@gmail.com>
2026-01-20 13:04:53 +0100

    Add unit tests for CategoryService using AutoFixture and NSubsstitute

---
+++ MiniEcommerce.BusinessLogicLayer.Tests/CategoryServiceTestBase.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using MiniEcommerce.BusinessLogicLayer.Services;
+using MiniEcommerce.Contracts.Entities;
+using MiniEcommerce.Contracts.Interfaces;
+using NSubstitute;
+
+namespace MiniEcommerce.BusinessLogicLayer.Tests
+{
+    public class CategoryServiceTestBase
+    {
+        protected readonly IFixture Fixture;
+        protected readonly IUnitOfWork UnitOfWork;
+        protected readonly IRepository<Category> CategoryRepository;
+
+        protected CategoryServiceTestBase()
+        {
+            Fixture = new Fixture().Customize(new AutoNSubstituteCustomization
+            {
+				ConfigureMembers = true
+            });
+
+            UnitOfWork = Fixture.Create<IUnitOfWork>();
+            CategoryRepository = Fixture.Create<IRepository<Category>>();
+
+            UnitOfWork.Categories.Returns(CategoryRepository);
+        }
+
+        protected CategoryService CreateService()
+        {
+            return new CategoryService(UnitOfWork);
+        }
+    }
+}

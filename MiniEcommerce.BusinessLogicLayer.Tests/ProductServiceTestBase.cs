9407ed1f191d76c6db89746a50dcab6f979f95a1
Mahammad <mahammad.ismayilov.2025@gmail.com>
2026-01-27 15:40:50 +0100

    Add unit tests for CategoryService using AutoFixture and NSubstitute (#12)
    * Add unit tests for CategoryService using AutoFixture and NSubstitute
    
    * test: add unit tests for ProductService

---
+++ MiniEcommerce.BusinessLogicLayer.Tests/ProductServiceTestBase.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using MiniEcommerce.BusinessLogicLayer.Services;
+using MiniEcommerce.Contracts.Entities;
+using MiniEcommerce.Contracts.Interfaces;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.BusinessLogicLayer.Tests
+{
+    public class ProductServiceTestBase
+    {
+        protected readonly IFixture Fixture;
+        protected readonly IUnitOfWork UnitOfWork;
+        protected readonly IProductRepository ProductRepository;
+		protected readonly IRepository<Category> CategoryRepository;
+
+		protected ProductServiceTestBase()
+        {
+            Fixture = new Fixture().Customize(new AutoNSubstituteCustomization
+            {
+                ConfigureMembers = true
+            });
+
+            UnitOfWork = Fixture.Create<IUnitOfWork>();
+            ProductRepository = Fixture.Create<IProductRepository>();
+			CategoryRepository = Fixture.Create<IRepository<Category>>();
+			UnitOfWork.Categories.Returns(CategoryRepository);
+
+			UnitOfWork.Products.Returns(ProductRepository);
+
+		}
+
+		protected ProductService CreateService()
+		{
+			return new ProductService(UnitOfWork);
+		}
+	}
+}
--- MiniEcommerce.BusinessLogicLayer/Interfaces/ICategoryService.cs
+++ MiniEcommerce.BusinessLogicLayer/Interfaces/ICategoryService.cs
@@ -11,6 +11,8 @@ public interface ICategoryService
     Task<IReadOnlyList<CategoryDto>> GetAllCategoriesAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
     Task<CategoryDto> GetCategoryByIdAsync(int categoryId, CancellationToken cancellationToken);
     Task<CategoryDto> CreateCategoryAsync(CreateCategoryDto dto, CancellationToken cancellationToken);
-    Task<CategoryDto> UpdateCategoryAsync(int categoryId, UpdateCategoryDto dto, CancellationToken cancellationToken);
+	Task<CategoryDto> GetCategoryByIdAsync(int categoryId, CancellationToken cancellationToken);
+
+	Task<CategoryDto> UpdateCategoryAsync(int categoryId, UpdateCategoryDto dto, CancellationToken cancellationToken);
     Task<CategoryDto> DeleteCategoryAsync(int categoryId, CancellationToken cancellationToken);
 }   

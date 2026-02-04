df94047531eebf4b9057b30a494076848e97e172
Mahammad Ismayilov <mahammad.ismayilov.2025@gmail.com>
2026-02-04 16:12:39 +0100

    resolve nullable mismatch

--- MiniEcommerce.Contracts/Interfaces/IUserRepository.cs
+++ MiniEcommerce.Contracts/Interfaces/IUserRepository.cs
@@ -9,6 +9,6 @@ namespace MiniEcommerce.Contracts.Interfaces
     {
         void Add(User user);
 
-        Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
     }
 }

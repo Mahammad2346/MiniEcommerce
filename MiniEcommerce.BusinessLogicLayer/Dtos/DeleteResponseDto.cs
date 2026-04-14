fe7eb9905bf7bd85af0283115dc6962c12fc9a62
Mahammad Ismayilov <mahammad.ismayilov.2025@gmail.com>
2026-04-14 17:15:46 +0200

    resolve nullable warning in DeleteResponseDto

--- MiniEcommerce.BusinessLogicLayer/Dtos/DeleteResponseDto.cs
+++ MiniEcommerce.BusinessLogicLayer/Dtos/DeleteResponseDto.cs
@@ -7,5 +7,5 @@ namespace MiniEcommerce.BusinessLogicLayer.Dtos;
 public class DeleteResponseDto
 {
 	public bool Success { get; set; }	
-	public string Message { get; set; }
+	public required string Message { get; set; }
 }

bba17c7408dbc29eb6f24a981afebb4f8c5bc9ec
Mahammad Ismayilov <mahammad.ismayilov.2025@gmail.com>
2026-05-25 05:39:49 +0200

    fix: resolve review and fix sonar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
-
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+import { authConfig } from './config'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
